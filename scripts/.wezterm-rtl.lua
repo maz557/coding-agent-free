e8888238a949e407a875c2541c7b8666e566f3d2
maz557 <maz557@gmail.com>
2026-07-06 12:26:40 +0330

    fix: correct bidi_direction value to AutoRightToLeft (was AutoRTL)

--- scripts/.wezterm-rtl.lua
+++ scripts/.wezterm-rtl.lua
@@ -3,6 +3,6 @@ local config = wezterm.config_builder()
 
 -- Enable RTL/BiDi support for Persian/Arabic (right-to-left) text
 config.bidi_enabled = true
-config.bidi_direction = 'AutoRTL'
+config.bidi_direction = 'AutoRightToLeft'
 
 return config
